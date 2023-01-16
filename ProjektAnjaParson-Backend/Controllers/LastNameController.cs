970eefb32f76a3a6fc68e8c40cf078c5f5e4fa92
MohamedELY <Mohamed.el.yahiaoui@outlook.com>
2023-01-16 13:28:29 +0100

    Added Controller to every Class

--- ProjektAnjaParson-Backend/Controllers/LastNameController.cs
+++ ProjektAnjaParson-Backend/Controllers/LastNameController.cs
@@ -1,45 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ProjektAnjaParson_Backend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : ControllerBase
+    public class LastNameController : ControllerBase
     {
-        // GET: api/Users
+        // GET: api/<LastNameController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/Users
+        // GET api/<LastNameController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return  $"value: {id}";
+            return "value";
         }
 
-        // POST api/Users
+        // POST api/<LastNameController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
-
         }
 
-        // PUT api/Users/5
+        // PUT api/<LastNameController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
-
         }
 
-        // DELETE api/Users/5
+        // DELETE api/<LastNameController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-
         }
     }
 }
