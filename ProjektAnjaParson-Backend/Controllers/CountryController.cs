7aa5ee3cc864b214fc36045ba79321b09aad071d
Vidar Fröberg Tinell <vidar.tinell@gmail.com>
2023-01-17 09:36:47 +0100

    Country Controller done execpt delete

--- ProjektAnjaParson-Backend/Controllers/CountryController.cs
+++ ProjektAnjaParson-Backend/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjektAnjaParson_Backend.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,34 +11,64 @@ namespace ProjektAnjaParson_Backend.Controllers
     {
         // GET: api/<CountryController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Country> Get()
         {
-            return new string[] { "value1", "value2" };
+            var data = new List<Country>();
+            using (var db = new ApdatabaseContext())
+            {
+                data = db.Countries.ToList();
+            }
+            return data;
         }
+
 
         // GET api/<CountryController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Country Get(int id)
         {
-            return "value";
+            var data = new Country();
+            using (var db = new ApdatabaseContext())
+            {
+                data = db.Countries.SingleOrDefault(c => c.Id == id);
+            }
+            return data;
         }
 
         // POST api/<CountryController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            using (var db = new ApdatabaseContext())
+            {
+                var data = db.Countries;
+                data.Add(new Country() { Name = value });
+                db.SaveChanges();
+            }
         }
 
         // PUT api/<CountryController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            using (var db = new ApdatabaseContext())
+            {
+                var data = db.Countries;
+
+                var selected = data.SingleOrDefault(c => c.Id == id);
+                if (selected != null)
+                {
+                    selected.Name = value;
+                    db.SaveChanges();
+                }
+            }
         }
+
 
         // DELETE api/<CountryController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+
         }
     }
 }
