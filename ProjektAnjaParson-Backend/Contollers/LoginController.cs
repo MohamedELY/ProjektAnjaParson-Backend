79f0ccef79ac131bb2b794cf1c6cdfbcd7b6de25
MohamedELY <Mohamed.el.yahiaoui@outlook.com>
2023-02-02 14:14:50 +0100

    Added Hash Class and Login Controller

---
+++ ProjektAnjaParson-Backend/Contollers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektAnjaParson_Backend.DataModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjektAnjaParson_Backend.Contollers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET api/<LoginController>/5
+        [HttpGet("{username}/{password}")]
+        public CUser Get(string username, string password)
+        {
+            List<CUser> users = new List<CUser>();
+            UserController caller = new UserController();
+
+
+            users = caller.Get();
+
+            foreach (var user in users)
+            {
+                if(user.Username == username && user.Password == password )
+                    return user;
+            }
+            return new CUser();
+        }
+    }
+}
---
+++ ProjektAnjaParson-Backend/Security/Hash.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ProjektAnjaParson_Backend.Security
+{
+
+    public static class Hash
+    {
+        public static string Execute(string rawData)
+        {
+            // Create a SHA256 
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                //Get bytes from string and hash
+                byte[] securedData = mySHA256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < securedData.Length; i++)
+                {
+                    builder.Append(securedData[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+    }
+}
