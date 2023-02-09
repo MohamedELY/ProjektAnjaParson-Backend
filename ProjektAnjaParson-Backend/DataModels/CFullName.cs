13e8bea152e27cb8d9fc420cf69e53d982340436
Hector <56518744+hecwik@users.noreply.github.com>
2023-02-09 10:00:44 +0100

    Comments added to clarify changes

--- ProjektAnjaParson-Backend/DataModels/CFullName.cs
+++ ProjektAnjaParson-Backend/DataModels/CFullName.cs
@@ -23,46 +23,48 @@ namespace ProjektAnjaParson_Backend.DataModels
             {
                 // Add first and lastname to database
 
-                /*var fisrtNameController = new FirstNameController();
+                /*var firstNameController = new FirstNameController();
                 var lastNameController = new LastNameController();
-                fisrtNameController.Post(firstName);
+                firstNameController.Post(firstName);
                 lastNameController.Post(lastName);*/
 
+                // FirstName POST
                 var existFname = db.FirstNames.SingleOrDefault(c => c.FirstName1.ToLower() == firstName.ToLower());
                 if (existFname == null)
                 {
                     db.FirstNames.Add(new FirstName() { FirstName1 = firstName });
-
                     db.SaveChanges();
                 }
                 
                 Console.WriteLine("First Name Has been Saved to DB");
 
+                // LastName POST
                 var existLname = db.LastNames.SingleOrDefault(c => c.LastName1.ToLower() == lastName.ToLower());
                 if (existLname == null)
                 {
                     db.LastNames.Add(new LastName() { LastName1 = lastName });
-
                     db.SaveChanges();
                 }
 
                 // Add fullname to database
 
-                /*var firstNameId = fisrtNameController.Get(firstName);
+                /*var firstNameId = firstNameController.Get(firstName);
                 var lastNameId = lastNameController.Get(lastName);
                 var fullNameController = new FullNameController();
                 fullNameController.Post(lastNameId.Id, firstNameId.Id);*/
                 
-                var getFirstNameByName = db.FirstNames.SingleOrDefault(c => c.FirstName1 == firstName);
+                // FirstName GET
+                var getFirstName = db.FirstNames.SingleOrDefault(c => c.FirstName1 == firstName);
                 Console.WriteLine("Retrieving First Name From DB");
-                var getLastNameByName = db.LastNames.SingleOrDefault(c => c.LastName1 == lastName);
+                // LastName GET
+                var getLastName = db.LastNames.SingleOrDefault(c => c.LastName1 == lastName);
                 Console.WriteLine("Retrieving Last Name From DB");
-                var existsFullName = db.FullNames.SingleOrDefault(c => c.FirstNameId == getFirstNameByName.Id && c.LastNameId == getLastNameByName.Id);
+
+                // FullName POST
+                var existsFullName = db.FullNames.SingleOrDefault(c => c.FirstNameId == getFirstName.Id && c.LastNameId == getLastName.Id);
                 if (existsFullName == null)
                 {
-                    var data = db.FullNames;
-                    data.Add(new FullName() { FirstNameId = getFirstNameByName.Id, LastNameId = getLastNameByName.Id });
-
+                    db.FullNames.Add(new FullName() { FirstNameId = getFirstName.Id, LastNameId = getLastName.Id });
                     db.SaveChanges();
                 }
 
